ce3d87b14f2bc8c693e880b867c7b3716628a830
Ege Baran Cakan <egebaranc@gmail.com>
2022-08-07 13:28:54 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("bir sayı giriniz");
+        int sayi=int.Parse(Console.ReadLine());
+        int sayac=1;
+        int toplam=0;
+        while(sayac<=sayi)
+        {
+           toplam+=sayac;
+           sayac++;
+        }
+        Console.WriteLine(toplam/sayi);
+        char character='a';
+        while(character<'z')
+        {
+            Console.WriteLine(character);
+            character++;
+        }
+        Console.WriteLine("ForEach");
+        string[] arabalar={"BMW","Ford","Toyota","Nissan"};
+        foreach (var item in arabalar)
+        {
+            Console.WriteLine(arabalar);
+        }
+    }
+}
